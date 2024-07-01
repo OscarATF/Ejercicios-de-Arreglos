cb982004937413b8bc12f52d8efe2fd0f458f1ab
Oscar <oticonaf@unjbg.edu.pe>
2024-07-01 11:48:34 -0500

    Agrega Ejercicio_3 (ocurrencia de una letra) de Ejercicios de Arreglos

---
+++ Ejercicio_3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void ponermayus(string cadena) {
+	int n=0,cont=0;
+	while (cadena[n]!='\0') {
+		n++;
+	}
+	for (int i=0;i<n;i++) {
+		if (cadena[i]=='o') {
+			cont++;
+		}
+	}
+	cout<<"La letra 'o' se repite "<<cont<<" veces.";
+}
+int main() {
+	string array;
+	array="Hola a todos";
+	cout<<"Contar la ocurrencia de una letra especifica en una cadena de entrada"<<endl;
+	cout<<endl<<array<<endl<<endl;
+	ponermayus(array);
+	return 0;
+}
