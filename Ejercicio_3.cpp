8a7f96504d7d625a66b5b0780aa0f1c42eafa1ec
Oscar <oticonaf@unjbg.edu.pe>
2024-07-01 23:37:39 -0500

    Actualiza Ejercicio_3 agrega entrada de oracion y de letra especifica

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -2,23 +2,26 @@
 #include <string.h>
 using namespace std;
 
-void ocurrencia(string cadena) {
+void ocurrencia(string cadena,char letraEsp) {
 	int n=0,cont=0;
 	while (cadena[n]!='\0') {
 		n++;
 	}
 	for (int i=0;i<n;i++) {
-		if (cadena[i]=='o') {
+		if (cadena[i]==letraEsp) {
 			cont++;
 		}
 	}
-	cout<<"La letra 'o' se repite "<<cont<<" veces.";
+	cout<<"La letra "<<letraEsp<<" se repite "<<cont<<" veces.";
 }
 int main() {
 	string array;
-	array="Hola a todos";
-	cout<<"Contar la ocurrencia de una letra especifica en una cadena de entrada"<<endl;
-	cout<<endl<<array<<endl<<endl;
-	ocurrencia(array);
+	char letra;
+	cout<<"Ingrese una oracion:"<<endl;
+	getline(cin,array);
+	cout<<"Ingrese la letra especifica:"<<endl;
+	cin>>letra;
+	cout<<"Contar la ocurrencia de la letra "<<"'"<<letra<<"'"<<" en una cadena de entrada"<<endl;
+	ocurrencia(array,letra);
 	return 0;
 }
